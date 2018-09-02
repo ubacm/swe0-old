e40f688a7735e4dee076b284932cfabf849e0cbf
Stephen James <stephenorjames@gmail.com>
2018-09-02 16:46:17 -0400

    Install Flask-Script
    This will help with making easily runnable sub-commands in the future,
    such as for testing and database migrations.

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask_script import Manager
+
+from swe0 import app
 
 
-app = Flask(__name__)
+manager = Manager(app)
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
