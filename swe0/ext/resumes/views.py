754ed827e19d3816b77a7005ac766d53cc0a9c34
Stephen James <stephenorjames@gmail.com>
2018-10-03 11:29:07 -0400

    Keep track of resume uploads in database

--- swe0/ext/resumes/views.py
+++ swe0/ext/resumes/views.py
@@ -1,41 +1,48 @@
 import os
 
-from flask import abort, redirect, render_template, request, send_file, url_for
+from flask import abort, redirect, render_template, request, send_file
 from flask_login import current_user, login_required
-from werkzeug.utils import secure_filename
 
-from swe0 import app
+from swe0 import db
 from . import resumes_blueprint
+from .models import Resume
 
 
 @resumes_blueprint.route('')
 @login_required
 def index():
-    return render_template('index.html')
+    resume = Resume.query.filter_by(user=current_user).first()
+    return render_template('index.html', resume=resume)
 
 
 @resumes_blueprint.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload():
     if request.method == 'POST' and 'resume' in request.files:
-        resume = request.files['resume']
-        if resume.filename.lower().endswith('.pdf'):
-            resume.save(_get_resume_file_path(current_user))
-            return redirect(url_for('.view'))
-
+        file = request.files['resume']
+        if file.filename.lower().endswith('.pdf'):
+            resume = Resume.query.filter_by(user=current_user).first()
+            new_filename = Resume.generate_filename()
+            if resume is not None:
+                # Delete existing resume.
+                os.remove(resume.file_path)
+            else:
+                # No existing resume, so create a new one.
+                resume = Resume(user=current_user)
+                db.session.add(resume)
+            resume.filename = new_filename
+            file.save(resume.file_path)
+            db.session.commit()
+            return redirect(resume.url_path)
     return render_template('upload_resume.html')
 
 
-@resumes_blueprint.route('/view')
+@resumes_blueprint.route('/view/<filename>')
 @login_required
-def view():
-    try:
-        return send_file(_get_resume_file_path(current_user))
-    except FileNotFoundError:
+def view(filename):
+    resume = Resume.query.filter_by(filename=filename).first()
+    if resume is None:
         abort(404)
-
-
-def _get_resume_file_path(user):
-    email_local, email_domain = user.email.split('@')
-    filename = '{}@{}'.format(secure_filename(email_local), secure_filename(email_domain + '.pdf'))
-    return os.path.join(app.config['UPLOAD_FOLDER'], 'resumes', filename)
+    if current_user != resume.user and not current_user.is_admin:
+        abort(403)
+    return send_file(resume.file_path)
