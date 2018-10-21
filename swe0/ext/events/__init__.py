fd6527028b7fb53aadd9db1ba24a7e060fa01c8d
Stephen James <stephenorjames@gmail.com>
2018-10-21 12:59:23 -0400

    Scope API endpoints under /<extension>/api for now

--- swe0/ext/events/__init__.py
+++ swe0/ext/events/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 
 
 events_blueprint = Blueprint('events', __name__)
-events_api = Api(events_blueprint)
+events_api = Api(events_blueprint, prefix='/api')
 
 
 # Ensure these are available when the blueprint is being registered.
