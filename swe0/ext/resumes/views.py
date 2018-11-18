233adb8af49d6e18e4e4d55f1c95a53b9f0bdcca
Stephen James <stephenorjames@gmail.com>
2018-11-18 14:07:47 -0500

    Allow admins to download all resumes as zip

--- swe0/ext/resumes/views.py
+++ swe0/ext/resumes/views.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+import io
 import os
+import zipfile
 
 from flask import abort, redirect, render_template, request, send_file, url_for
 from flask_login import current_user, login_required
@@ -14,6 +17,22 @@ from .models import Resume
 def index():
     resume = Resume.query.filter_by(user=current_user).first()
     return render_template('resumes/index.html', resume=resume)
+
+
+@resumes_blueprint.route('/download')
+@login_required
+def download():
+    if not current_user.is_admin:
+        abort(403)
+    resume_list = Resume.query.join(User, Resume.user).order_by(User.email).all()
+    buffer = io.BytesIO()
+    with zipfile.ZipFile(buffer, 'a', zipfile.ZIP_DEFLATED) as archive:
+        for resume in resume_list:
+            resume_arcname = '{user.name}_{user.email}_resume.pdf'.format(user=resume.user)
+            archive.write(resume.file_path, resume_arcname)
+    buffer.seek(0)
+    zip_filename = datetime.now().strftime('resumes_%Y-%m-%d-%H-%M.zip')
+    return send_file(buffer, attachment_filename=zip_filename, as_attachment=True)
 
 
 @resumes_blueprint.route('/upload', methods=['GET', 'POST'])
